7de25a68b5b2eaf935c3281f3a67722315f3e8dc
Luca Pandini <luca.pandini@lastminute.com>
2018-10-20 16:46:19 +0200

    scan case with folded lines

--- src/__tests__/scanner.test.js
+++ src/__tests__/scanner.test.js
@@ -8,6 +8,14 @@ DTEND:20130906T150000Z
 END:VEVENT
 END:VCALENDAR`;
 
+const TEXT_WITH_FOLDED_LINE_BY_A_SPACE =
+`DESCRIPTION:a line can 
+ be folded`;
+
+const TEXT_WITH_FOLDED_LINES_BY_A_SPACE_OR_TAB =
+'DESCRIPTION:a line can \n be folded by a space\nDESCRIPTION:or by a \n\ttab';
+
+
 describe('scan', () => {
   it('with one event', () => {
     expect(scan(CALENDAR_WITH_ONE_EVENT)).toEqual(
@@ -18,6 +26,23 @@ describe('scan', () => {
         {kind: 'DTEND', value: '20130906T150000Z'},
         {kind: 'END', value: 'VEVENT'},
         {kind: 'END', value: 'VCALENDAR'}
+      ]
+    );
+  });
+
+  it('with line folded by space', () => {
+    expect(scan(TEXT_WITH_FOLDED_LINE_BY_A_SPACE)).toEqual(
+      [
+        {kind: 'DESCRIPTION', value: 'a line can be folded'}
+      ]
+    );
+  });
+
+  it('with line folded by mixed whitespaces', () => {
+    expect(scan(TEXT_WITH_FOLDED_LINES_BY_A_SPACE_OR_TAB)).toEqual(
+      [
+        {kind: 'DESCRIPTION', value: 'a line can be folded by a space'},
+        {kind: 'DESCRIPTION', value: 'or by a tab'}
       ]
     );
   });
--- src/scanner.js
+++ src/scanner.js
@@ -1,5 +1,9 @@
+function unfold(text) {
+  return text.replace(/\n[\s|\t]/g, '');
+};
+
 export default function scan(text) {
-  const lines = text.split('\n');
+  const lines = unfold(text).split('\n');
   return lines.map(l => {
     const tokens = l.split(':');
     return {
