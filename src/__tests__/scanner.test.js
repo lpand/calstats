4cd378a7b847479e7097e156fa3adf57b85feb6d
lpand <luca1.pandini@gmail.com>
2018-10-20 15:45:41 +0200

    init

---
+++ src/__tests__/scanner.test.js
@@ -0,0 +1,24 @@
+import scan from '../scanner';
+
+const CALENDAR_WITH_ONE_EVENT =
+`BEGIN:VCALENDAR
+BEGIN:VEVENT
+DTSTART:20130906T143000Z
+DTEND:20130906T150000Z
+END:VEVENT
+END:VCALENDAR`;
+
+describe('scan', () => {
+  it('with one event', () => {
+    expect(scan(CALENDAR_WITH_ONE_EVENT)).toEqual(
+      [
+        {kind: 'BEGIN', value: 'VCALENDAR'},
+        {kind: 'BEGIN', value: 'VEVENT'},
+        {kind: 'DTSTART', value: '20130906T143000Z'},
+        {kind: 'DTEND', value: '20130906T150000Z'},
+        {kind: 'END', value: 'VEVENT'},
+        {kind: 'END', value: 'VCALENDAR'},
+      ]
+    );
+  });
+});
